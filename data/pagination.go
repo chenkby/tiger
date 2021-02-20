5f0a8684b3c5381b0ac8fd5197ed1f9d1e2b327d
chenkby <99912250@qq.com>
2021-02-20 17:12:25 +0800

    Create pagination.go
    init

---
+++ data/pagination.go
@@ -0,0 +1,33 @@
+package data
+
+import "math"
+
+type Paging struct {
+	CurrentPage int // 当前页码
+	PageSize    int // 每页显示多少条
+	TotalCount  int // 总条数
+	PageCount   int // 总共多少页
+	Offset      int `json:"-"` // 起始页
+}
+
+func NewPagination(page, pageSize, total int) *Paging {
+	if page < 1 {
+		page = 1
+	}
+	if pageSize < 1 {
+		pageSize = 10
+	}
+	pageCount := int(math.Ceil(float64(total) / float64(pageSize)))
+
+	if page > pageCount {
+		page = pageCount
+	}
+	offset := pageSize * (page - 1)
+	paging := new(Paging)
+	paging.CurrentPage = page
+	paging.PageSize = pageSize
+	paging.TotalCount = total
+	paging.PageCount = pageCount
+	paging.Offset = offset
+	return paging
+}
